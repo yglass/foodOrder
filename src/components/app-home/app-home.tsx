9a8e6f9e2ff5be9c4e70080b989f29bb3c5f5c83
Rami Mustaklem (mr8118) <mr8118@intl.att.com>
2019-11-03 15:32:17 +0200

    Final stylings and changes

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -7,23 +7,35 @@ import { Component, h } from '@stencil/core';
 export class AppHome {
 
   render() {
-    return (
+    return [
+      <header>
+        <h1>Your top recommendations</h1>
+      </header>,
       <div class='app-home'>
         <stencil-route-link url='/order/Margarita'>
-          <dominos-tile pizza-title="Double Deal Medium" img="/assets/images/double-deal-medium.jpg" price="7.99$" description="Double deal medium pizza 2-toppings pizzas">
+          <dominos-tile pizza-title="Double Deal Medium"
+                        img="/assets/images/double-deal-medium.jpg"
+                        price="7.99$"
+                        description="Double deal medium pizza 2-toppings pizzas">
           </dominos-tile>
         </stencil-route-link>
 
         <stencil-route-link url='/order/Vegetables'>
-          <dominos-tile pizza-title="Large 3 Toppings" img="/assets/images/large-3-toppings.jpg" price="8.99$" description="Large 3 toppings pizza">
+          <dominos-tile pizza-title="Large 3 Toppings"
+                        img="/assets/images/large-3-toppings.jpg"
+                        price="8.99$"
+                        description="Large 3 toppings pizza">
           </dominos-tile>
         </stencil-route-link>
 
         <stencil-route-link url='/order/Chicken'>
-          <dominos-tile pizza-title="Unlimited Medium" img="/assets/images/unlimited-medium.jpg" price="9.99$" description="Unlimited Medium 2 toppings">
+          <dominos-tile pizza-title="Unlimited Medium"
+                        img="/assets/images/unlimited-medium.jpg"
+                        price="9.99$"
+                        description="Unlimited Medium 2 toppings">
           </dominos-tile>
         </stencil-route-link>
       </div>
-    );
+    ];
   }
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -10,6 +10,15 @@ export class HeaderComponent {
     return [
       <div>
         <img class="header-logo" src="/assets/icon/logo.png" title='Dominos mini app' alt='Dominos mini app'></img>
+        <div class="address">
+          Order to:
+          <br/>
+          1600 Pennsylvania Ave NW,
+          <br/>
+          Washington,
+          <br/>
+          DC 20500
+        </div>
       </div>
     ];
   }
